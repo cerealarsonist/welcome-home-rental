ce4dd99d4120e2f6f9318261172ccf83660a069e
Johan Kiane Vito <your_github_email@example.com>
2026-04-14 23:56:33 +0800

    Set ALLOWED_HOSTS for Render deployment

--- welcome_home/welcome_home/settings.py
+++ welcome_home/welcome_home/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-fallback-key")
 DEBUG = False
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["welcome-home-fxye.onrender.com", "localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     'daphne',
